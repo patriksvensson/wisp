75060e4e612640eec8c3d958d06f6445f4186cb9
Patrik Svensson <patrik@patriksvensson.se>
2024-03-06 00:18:45 +0100

    Add PNG "Up" predictor for Flate filter

--- src/Wisp/Filters/FlateFilter.cs
+++ src/Wisp/Filters/FlateFilter.cs
@@ -12,15 +12,18 @@ public sealed class FlateFilter : Filter
         // Run the deflate algorithm
         var bytes = Deflate(data);
 
-        var predictor = parameters?.GetInt32(CosNames.Predictor) ?? 1;
-        if (predictor != 1)
+        var settings = GetPredictorSettings(parameters);
+        if (settings.Predictor == 1)
         {
-            // TODO: Support PNG encoding
-            throw new NotSupportedException(
-                "Additional decoding is required, but not yet supported");
+            return bytes;
         }
 
-        return bytes;
+        if (settings.Predictor == 2)
+        {
+            throw new NotSupportedException("TIFF predictor not supported");
+        }
+
+        return PngDecoder.Decode(bytes, settings.Columns, settings.Colors, settings.BitsPerComponent);
     }
 
     private static byte[] Deflate(byte[] data)
@@ -45,5 +48,110 @@ public sealed class FlateFilter : Filter
         }
 
         return output.ToArray();
+    }
+
+    private static (int Predictor, int Columns, int Colors, int BitsPerComponent)
+        GetPredictorSettings(CosDictionary? parameters)
+    {
+        var predictor = parameters?.GetInt32(CosNames.Predictor) ?? 1;
+        var columns = parameters?.GetInt32(CosNames.Columns) ?? 1;
+        var colors = parameters?.GetInt32(CosNames.Colors) ?? 1;
+        var bits = parameters?.GetInt32(CosNames.BitsPerComponent) ?? 8;
+
+        return (predictor, columns, colors, bits);
+    }
+
+    private static class PngDecoder
+    {
+        public static byte[] Decode(byte[] bytes, int columns, int colors, int bitsPerComponent)
+        {
+            var bytesPerRow = ((colors * columns * bitsPerComponent) + 7) / 8;
+
+            var reader = new BinaryReader(new MemoryStream(bytes));
+            var writer = new MemoryStream(bytes.Length);
+
+            var previous = default(byte[]);
+
+            while (true)
+            {
+                var filter = reader.Read();
+                if (filter < 0)
+                {
+                    return writer.ToArray();
+                }
+
+                var current = new byte[bytesPerRow];
+                ReadBytes(reader, current, bytesPerRow);
+
+                if (filter == 0)
+                {
+                    // NONE
+                }
+                else if (filter == 1)
+                {
+                    // SUB
+                    throw new NotSupportedException("Unsupported filter: PngSub");
+                }
+                else if (filter == 2)
+                {
+                    // UP
+                    if (previous != null)
+                    {
+                        for (var i = 0; i < bytesPerRow; i++)
+                        {
+                            current[i] += previous[i];
+                        }
+                    }
+                }
+                else if (filter == 3)
+                {
+                    // AVERAGE
+                    throw new NotSupportedException("Unsupported filter: PngAverage");
+                }
+                else if (filter == 4)
+                {
+                    // PAETH
+                    throw new NotSupportedException("Unsupported filter: PngPaeth");
+                }
+                else if (filter == 5)
+                {
+                    // PAETH
+                    throw new NotSupportedException("Unsupported filter: PngOptimum");
+                }
+                else
+                {
+                    // UNKNOWN
+                    throw new NotSupportedException("Encountered unknown PNG filter during decoding");
+                }
+
+                // Write the current row to the stream
+                writer.Write(current);
+
+                // Swap streams
+                previous = current;
+            }
+        }
+
+        // TODO: Rewrite
+        public static void ReadBytes(BinaryReader reader, byte[] buffer, int count)
+        {
+            if (count < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var off = 0;
+            var n = 0;
+            while (n < count)
+            {
+                int read = reader.Read(buffer, off + n, count - n);
+                if (read <= 0)
+                {
+                    throw new EndOfStreamException();
+                }
+
+                n += read;
+            }
+        }
     }
 }
