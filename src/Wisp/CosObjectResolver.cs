572674828e411a7fda7e354c9973bb5ce759979d
Patrik Svensson <patrik@patriksvensson.se>
2024-03-13 01:29:42 +0100

    Try get object stream object from cache

--- src/Wisp/CosObjectResolver.cs
+++ src/Wisp/CosObjectResolver.cs
@@ -31,26 +31,35 @@ internal sealed class CosObjectResolver : IDisposable
                 var streamObjectXRef = _xRefTable.GetXRef(streamXref.StreamId) as CosIndirectXRef;
                 Debug.Assert(streamObjectXRef != null, "Could not get xref to stream object");
 
+                // Ensure that the stream has a position
                 if (streamObjectXRef.Position == null)
                 {
                     throw new InvalidOperationException("Object in object stream should exist in cache");
                 }
 
-                // Parse the object stream
-                _parser.Seek(streamObjectXRef.Position.Value, SeekOrigin.Begin);
-                var streamObject = _parser.Parse() as CosObject;
-                Debug.Assert(streamObject != null, "Could not find an object at the stream position");
-                var objectStream = streamObject.Object as CosObjectStream;
+                // Is the stream itself in the cache?
+                // Don't try to resolve the stream object; that will lead to a stack overflow
+                var ownerObject = cache.Get(streamObjectXRef.Id, CosResolveFlags.NoResolve);
+                if (ownerObject == null)
+                {
+                    // Parse the object stream
+                    _parser.Seek(streamObjectXRef.Position.Value, SeekOrigin.Begin);
+                    ownerObject = _parser.Parse() as CosObject;
+                    Debug.Assert(ownerObject != null, "Could not find an object at the stream position");
+                }
+
+                // Ensure the primitive is an object stream
+                var objectStream = ownerObject.Object as CosObjectStream;
                 Debug.Assert(objectStream != null, "Object was not an object stream");
 
                 // Get the object within the stream
-                var item = objectStream.GetObjectByIndex(cache, streamXref.Index);
-                if (item == null)
+                var objectStreamItem = objectStream.GetObjectByIndex(cache, streamXref.Index);
+                if (objectStreamItem == null)
                 {
                     return null;
                 }
 
-                return (streamObject, item);
+                return (ownerObject, objectStreamItem);
 
             case CosIndirectXRef indirectXRef:
                 if (indirectXRef.Position == null)
