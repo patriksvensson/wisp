9d5c29904c5373798c8f0d6e7678f654bdeb633a
Patrik Svensson <patrik@patriksvensson.se>
2024-03-20 08:54:18 +0100

    Clean up tests

--- src/Wisp.Tests/CosDocumentTests.Parser.cs
+++ src/Wisp.Tests/CosDocumentTests.Parser.cs
@@ -1,56 +1,47 @@
+using JetBrains.Annotations;
+
 namespace Wisp.Tests;
 
 public sealed partial class CosDocumentTests
 {
+    [UsedImplicitly]
     public sealed class Parsing
     {
         public sealed class Strings
         {
             [Fact]
-            public void Should_Respect_Escaped_Closing_Parentheses_In_Strings()
+            public void Escaped_Closing_Parenthesis()
             {
                 // Given
-                var doc = new CosDocument();
-                doc.Objects.Set(new CosObject(new CosObjectId(1, 0), new CosDictionary()
-                {
-                    { new CosName("C"), new CosName("SC.12.303125") },
-                    { new CosName("K"), new CosInteger(73) },
-                    { new CosName("Lang"), new CosString(")q") },
-                }));
-                var stream = new MemoryStream();
-                doc.Save(stream, leaveOpen: true);
-
-                // When
-                var newDocument = CosDocument.Open(stream);
-                var obj = newDocument.Objects.Get(new CosObjectId(1, 0));
+                var obj = CosParserFixture.WriteAndParse(
+                    new CosDictionary()
+                    {
+                        { new CosName("C"), new CosName("SC.12.303125") },
+                        { new CosName("K"), new CosInteger(73) },
+                        { new CosName("Lang"), new CosString(")q") },
+                    });
 
                 // Then
                 obj.ShouldNotBeNull();
-                obj.Object.ShouldBeOfType<CosDictionary>().And()["Lang"]
+                obj.ShouldBeOfType<CosDictionary>().And()["Lang"]
                     .ShouldBeOfType<CosString>().ShouldHaveValue(")q");
             }
 
             [Fact]
-            public void Should_Respect_Escaped_Opening_Parentheses_In_Strings()
+            public void Escaped_Opening_Parenthesis()
             {
                 // Given
-                var doc = new CosDocument();
-                doc.Objects.Set(new CosObject(new CosObjectId(1, 0), new CosDictionary()
-                {
-                    { new CosName("C"), new CosName("SC.12.303125") },
-                    { new CosName("K"), new CosInteger(73) },
-                    { new CosName("Lang"), new CosString("(q") },
-                }));
-                var stream = new MemoryStream();
-                doc.Save(stream, leaveOpen: true);
-
-                // When
-                var newDocument = CosDocument.Open(stream);
-                var obj = newDocument.Objects.Get(new CosObjectId(1, 0));
+                var obj = CosParserFixture.WriteAndParse(
+                    new CosDictionary()
+                    {
+                        { new CosName("C"), new CosName("SC.12.303125") },
+                        { new CosName("K"), new CosInteger(73) },
+                        { new CosName("Lang"), new CosString("(q") },
+                    });
 
                 // Then
                 obj.ShouldNotBeNull();
-                obj.Object.ShouldBeOfType<CosDictionary>().And()["Lang"]
+                obj.ShouldBeOfType<CosDictionary>().And()["Lang"]
                     .ShouldBeOfType<CosString>().ShouldHaveValue("(q");
             }
 
@@ -58,23 +49,17 @@ public sealed partial class CosDocumentTests
             public void Escaped_Backslash()
             {
                 // Given
-                var doc = new CosDocument();
-                doc.Objects.Set(new CosObject(new CosObjectId(1, 0), new CosDictionary()
-                {
-                    { new CosName("Lang"), new CosString("p\\") },
-                    { new CosName("C"), new CosName("SC.12.303125") },
-                    { new CosName("K"), new CosInteger(73) },
-                }));
-                var stream = new MemoryStream();
-                doc.Save(stream, leaveOpen: true);
-
-                // When
-                var newDocument = CosDocument.Open(stream);
-                var obj = newDocument.Objects.Get(new CosObjectId(1, 0));
+                var obj = CosParserFixture.WriteAndParse(
+                    new CosDictionary()
+                    {
+                        { new CosName("Lang"), new CosString("p\\") },
+                        { new CosName("C"), new CosName("SC.12.303125") },
+                        { new CosName("K"), new CosInteger(73) },
+                    });
 
                 // Then
                 obj.ShouldNotBeNull();
-                obj.Object.ShouldBeOfType<CosDictionary>().And()["Lang"]
+                obj.ShouldBeOfType<CosDictionary>().And()["Lang"]
                     .ShouldBeOfType<CosString>().ShouldHaveValue("p\\");
             }
         }
---
+++ src/Wisp.Tests/Fixtures/CosParserFixture.cs
@@ -0,0 +1,16 @@
+namespace Wisp.Testing.Fixtures;
+
+public static class CosParserFixture
+{
+    public static ICosPrimitive? WriteAndParse(ICosPrimitive primitive)
+    {
+        var doc = new CosDocument();
+        doc.Objects.Set(new CosObject(new CosObjectId(1, 0), primitive));
+        using var stream = new MemoryStream();
+        doc.Save(stream, leaveOpen: true);
+
+        // When
+        var newDocument = CosDocument.Open(stream);
+        return newDocument.Objects.Get(new CosObjectId(1, 0))?.Object;
+    }
+}
